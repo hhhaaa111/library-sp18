d86221c10f5016edff6cd159fe33c91378defef1
hhhaaa111 <sheltongeorge123abc@gmail.com>
2024-09-03 17:12:51 +0800

    第一次

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,76 @@
+public class ArrayDeque<T>{
+    private T[] items;
+    private int size;
+    public ArrayDeque(){
+        items = (T[]) new Object[8];
+        size = 0;
+    }
+
+    public void addFirst(T item){
+        T[] a = (T[])new Object(size+1);
+        a[0] = item;
+        System.arraycopy(array,1,a,1,size);
+        items = a;
+        size++;
+    }
+
+    public void resize(int size){
+        T[] a = (T[]) new Object(size*2);
+        System.arraycopy(items,0,a,0,size);
+        items = a;
+    }
+
+    public void addLast(T item){
+       if(size == items.length){
+          resize(size);
+       }
+       items[size] = item;
+       size++;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void printDeque(){
+       for(int i = 0; i < size; i++){
+           System.out.print(items[i]+" ");
+       }
+    }
+
+    public T removeFirst(){
+       if(size == 0){
+           return null;
+       }
+       T[] a = (T[]) new Object[size-1];
+       T first = items[0];
+       System.arraycopy(items,1,a,0,size-1);
+       items = a;
+       size--;
+       return first;
+    }
+
+    public T removeLast(){
+        if(size == 0){
+            return null;
+        }
+        T[] a = (T[]) new Object[size-1];
+        T last = items(size-1);
+        System.arraycopy(items,0,a,0,size-1);
+        items = a;
+        size--;
+        return last;
+    }
+
+    public T get(int index){
+       if(index < 0 || index >= size){
+           return null;
+       }
+       return items[index];
+    }
+
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,100 @@
+public class LinkedListDeque<T> {
+    public class Node<T> {
+        public Node prev;
+        public T item;
+        public Node<T> next;
+        public Node(T item, Node prev, Node next) {
+            this.item = item;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    Node sentinel;
+    int size;
+
+    public LinkedListDeque(){
+       sentinel = new Node(null, null, null);
+       sentinel.prev = sentinel;
+       sentinel.next = sentinel;
+       size = 0;
+    }
+
+    public void addFirst(T item){
+       Node first = new Node(item, sentinel, sentinel.next);
+       sentinel.next = first;
+       sentinel.next.prev = first;
+       size++;
+    }
+
+    public void addLast(T item){
+       Node last = new Node(item,sentinel.next,sentinel);
+       sentinel.prev = last;
+       sentinel.next.prev = last;
+       size++;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+      }//如果是错误的，会直接输出false，不需要写if，else了
+
+    public int size(){
+        return size;
+    }
+
+    public void printDeque(){
+       Node p = sentinel.next;
+       while(size != 0){
+           Sysytem.out.println(p.item + " ");
+           p = p.next;
+           szie--;
+       }
+    }
+
+    public T removeFirst(){
+        if(size == 0)
+            return null;
+        Node firstNode = sentinel.next;
+        sentinel.next = firstNode.next;
+        firstNode.next.prev = sentinel;
+        size--;
+        firstNode.prev = null;
+        firstNode.next = null;
+        return firstNode.item;
+    }
+
+    public T removeLast(){
+       if(size == 0)
+           return null;
+       Node lastNode = sentinel.prev;
+       lastNode.prev = sentinel;
+       sentinel.prev = lastNode.prev;
+       size--;
+       lastNode.prev = null;
+       lastNode.next = null;
+       return lastNode.item;
+    }
+
+    //迭代
+    public T get(int index){
+      int t = index + 1;
+      Node p = sentinel.next;
+      while(t!=0){
+          p = p.next;
+          t--;
+      }
+      return p.item;
+    }
+
+    //递归
+    public T getRecursive(int index){
+       private T getRecursivehelper(Node current, int index){
+           current = sentinel.next;
+           this.index = index;
+           if(index == 0){
+               return current.item;
+           }
+           return getRecursivehelper(current.next, index-1);
+        }
+    }
+}
